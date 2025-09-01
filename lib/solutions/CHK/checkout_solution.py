7d6b88a761367ce892ed521228f20f441426c2a8
sbx_user1051 <sbx_user1051@169.254.47.171>
2025-09-01 12:14:26 +0000

    Mon Sep 01 12:14:26 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,31 +1,24 @@
+class CheckoutSolution:
+# The 'checkout' function MUST be indented once to be inside the class
+    def checkout(self, skus):
+# All the code for the function MUST be indented again
+        prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
 
-        class CheckoutSolution:
-        def checkout(self, skus):
-# Price list from the rules
-           prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+        for item in skus:
+            if item not in prices:
+                return -1
 
+        total_bill = 0
 
-for item in skus:
-    if item not in prices:
-            return -1
-
-    total_bill = 0
-
-    count_A = skus.count('A')
-# Add the price for the special offer (groups of 3)
+        count_A = skus.count('A')
         total_bill += (count_A // 3) * 130
-# Add the price for any leftover 'A's
         total_bill += (count_A % 3) * prices['A']
 
-
-    count_B = skus.count('B')
-# Add the price for the special offer (groups of 2)
+        count_B = skus.count('B')
         total_bill += (count_B // 2) * 45
-# Add the price for any leftover 'B's
         total_bill += (count_B % 2) * prices['B']
 
-    total_bill += skus.count('C') * prices['C']
-    total_bill += skus.count('D') * prices['D']
-
+        total_bill += skus.count('C') * prices['C']
+        total_bill += skus.count('D') * prices['D']
 
         return total_bill
