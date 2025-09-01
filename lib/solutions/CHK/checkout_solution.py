0dff0a5a11dd110f389c0940729a35c02ac652d2
sbx_user1051 <sbx_user1051@169.254.47.171>
2025-09-01 12:05:26 +0000

    Mon Sep 01 12:05:26 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,45 +1,31 @@
 
 class CheckoutSolution:
+def checkout(self, skus):
+# Price list from the rules
+prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
 
-    # skus = unicode string
-    def checkout(self, skus):
-#  Errsa's pricebook
-price_book = {
-    'A': 50,
-    'B': 30,
-    'C': 20,
-    'D': 15,
-}
-
-special_offers = {
-    'A' : {'count' : 3, 'price' : 130},
-    'B' : {'count' : 2, 'price' : 45}
-}
 
 for item in skus:
-    if item not in price_book :
-        return -1
-    
-    item_counts = {
-        'A' : skus.count('A'),
-        'B' : skus.count('B'),
-        'C' : skus.count('C'),
-        'D' : skus.count('D')
-    }
+if item not in prices:
+return -1
+
 total_bill = 0
 
-count_A = item_counts['A']
-offer_A = special_offers['A']
-total_bill += (count_A // offer_A ['count'])* price_book['A']
+count_A = skus.count('A')
+# Add the price for the special offer (groups of 3)
+total_bill += (count_A // 3) * 130
+# Add the price for any leftover 'A's
+total_bill += (count_A % 3) * prices['A']
 
 
-count_B = item_counts['B']
-offer_B = special_offers['B']
-total_bill += (count_B // offer_B ['count'])* price_book['B']
+count_B = skus.count('B')
+# Add the price for the special offer (groups of 2)
+total_bill += (count_B // 2) * 45
+# Add the price for any leftover 'B's
+total_bill += (count_B % 2) * prices['B']
 
+total_bill += skus.count('C') * prices['C']
+total_bill += skus.count('D') * prices['D']
 
-total_bill += item_counts['C']* price_book['C']
-
-total_bill += item_counts['D']* price_book['D']
 
 return total_bill
