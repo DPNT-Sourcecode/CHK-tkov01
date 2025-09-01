d93c5aa15ec29bf259cb0a1221ef0ddb3252fb1b
sbx_user1051 <sbx_user1051@169.254.47.171>
2025-09-01 11:53:27 +0000

    Mon Sep 01 11:53:27 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,19 +3,43 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        raise NotImplementedError()
 #  Errsa's pricebook
-Errsas_price_book = {
+price_book = {
     'A': 50,
     'B': 30,
     'C': 20,
     'D': 15,
 }
 
-final_bill = 0
-for item in skus:
+special_offers = {
+    'A' : {'count' : 3, 'price' : 130},
+    'B' : {'count' : 2, 'price' : 45}
+}
 
-    if price is None:
-        return: -1
-final_bill += Errsas_price_book
-return final_bill
+for item in skus:
+    if item not in price_book :
+        return -1
+    
+    item_counts = {
+        'A' : skus.count('A'),
+        'B' : skus.count('B'),
+        'C' : skus.count('C'),
+        'D' : skus.count('D')
+    }
+total_bill = 0
+
+count_A = item_counts['A']
+offer_A = special_offers['A']
+total_bill += (count_A // offer_A ['count'])* price_book['A']
+
+
+count_B = item_counts['B']
+offer_B = special_offers['B']
+total_bill += (count_B // offer_B ['count'])* price_book['B']
+
+
+total_bill += item_counts['C']* price_book['C']
+
+total_bill += item_counts['D']* price_book['D']
+
+return total_bill
