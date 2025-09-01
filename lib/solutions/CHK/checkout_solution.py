bf01ca8bedab9bd3b9088fa869fda265017e3af4
sbx_user1051 <sbx_user1051@169.254.93.81>
2025-09-01 11:31:40 +0000

    Mon Sep 01 11:31:40 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,3 +4,18 @@ class CheckoutSolution:
     # skus = unicode string
     def checkout(self, skus):
         raise NotImplementedError()
+#  Errsa's pricebook
+Errsas_price_book = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15,
+}
+
+final_bill = 0
+for item in skus:
+
+    if price is None:
+        return: -1
+final_bill += Errsas_price_book
+return final_bill
